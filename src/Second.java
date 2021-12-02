2231071a6dd6d726d08c14da3f8f91ff42180855
ZEROCODE0521 <78863469+ZEROCODE0521@users.noreply.github.com>
2021-12-02 18:59:18 +0900

    4,5 Dan

---
+++ src/Second.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Second {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("구구단 중 출력할 단은? : ");
+		final int number = scanner.nextInt();
+		System.out.println("number : " + number);
+		
+		int result = number * 1;
+		System.out.println(result);
+		result = number * 2;
+		System.out.println(result);
+		result = number * 3;
+		System.out.println(result);
+		result = number * 4;
+		System.out.println(result);
+		result = number * 5;
+		System.out.println(result);
+		result = number * 6;
+		System.out.println(result);
+		result = number * 7;
+		System.out.println(result);
+		result = number * 8;
+		System.out.println(result);
+		result = number * 9;
+		System.out.println(result);
+	}
+
+}
