f73d22a69a05e47e2703f5247fb469ef926e0850
ZEROCODE0521 <78863469+ZEROCODE0521@users.noreply.github.com>
2021-12-03 11:53:18 +0900

    메서드를 이용하여 구현

---
+++ src/fifth.java
@@ -0,0 +1,28 @@
+
+public class fifth {
+
+	public static void main(String[] args) {
+		for (int j=2;j<10;j++) {
+			System.out.println(j + "단을 출력합니다.");
+			int[] result = caculate(j);
+			print(result);
+		}
+
+	}
+	
+	public static int[] caculate(int times) {
+		int[] result = new int[9];
+		
+		for (int i=0; i<result.length; i++) {
+			result[i] = times * (i+1);
+		}
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for(int i =0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+
+}
